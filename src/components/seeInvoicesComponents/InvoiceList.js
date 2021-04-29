0d2b9147b6cee6456bd587ba47d725f0c1c4e48b
Puh00 <alex.phu@live.com>
2021-04-29 12:54:32 +0200

    Can now pass invoice to PreviewInvoice

--- src/components/seeInvoicesComponents/InvoiceList.js
+++ src/components/seeInvoicesComponents/InvoiceList.js
@@ -1,84 +1,97 @@
-import {Table} from "react-bootstrap";
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import 'react-responsive-modal/styles.css';
-import {Modal} from 'react-responsive-modal';
+import { Table } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
 import PreviewInvoice from "./PreviewInvoice";
-import './pdfStyling.css'
+import "./pdfStyling.css";
 
+const InvoiceList = ({ props, invoiceList }) => {
+  const [open, setOpen] = useState(false);
+  const [currentInvoice, setCurrentInvoice] = useState({});
+  const [test, setTest] = useState("YES");
 
-const InvoiceList = ({props, invoiceList}) => {
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
 
-    const [open, setOpen] = useState(false);
-    const [currentInvoice, setCurrentInvoice] = useState({});
+  const sumOfProducts = (items) => {
+    let sum = 0;
+    items.forEach((product) => (sum += product.price * product.quantity));
+    return sum;
+  };
 
-    const onOpenModal = () => setOpen(true);
-    const onCloseModal = () => setOpen(false);
+  const goBackToHomePage = () => {
+    props.history.push("/");
+  };
 
-    const sumOfProducts = (items) => {
-        let sum = 0;
-        items.forEach(product => sum += product.price * product.quantity);
-        return sum;
-    }
+  // This is needed since setState is asynchronous, must wait for it to update
+  // before sending currentInvoice
+  useEffect(() => {
+    console.log("currentInvoice > " + JSON.stringify(currentInvoice));
+    onOpenModal();
+  }, [currentInvoice]);
 
-    const goBackToHomePage = () => {
-        props.history.push('/');
-    }
+  const loadPreview = (invoice) => {
+    setCurrentInvoice(invoice);
+    console.log("pre-invoice > " + JSON.stringify(invoice));
+    setTest("true");
+  };
 
-    const loadPreview = (invoice) => {
-        setCurrentInvoice(invoice);
-        onOpenModal()
-    }
+  return (
+    <div>
+      <button onClick={onOpenModal}>Open modal</button>
+      <Modal
+        open={open}
+        onClose={onCloseModal}
+        center
+        classNames={{
+          overlay: "customOverlay",
+          modal: "customModal",
+        }}
+      >
+        <PreviewInvoice  {...currentInvoice}/>
+      </Modal>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>Sent</th>
+            <th>Expires</th>
+            <th>Status</th>
+            <th>Serial Number</th>
+            <th>Buyer</th>
+            <th>Price</th>
+          </tr>
+        </thead>
 
-    return (
-        <div>
-            <button onClick={onOpenModal}>Open modal</button>
-            <Modal open={open} onClose={onCloseModal} center classNames={{
-                overlay: 'customOverlay',
-                modal: 'customModal',
-            }}>
-                <PreviewInvoice data={currentInvoice}/>
-            </Modal>
-            <Table striped bordered hover variant="dark">
-                <thead>
-                <tr>
-                    <th>Sent</th>
-                    <th>Expires</th>
-                    <th>Status</th>
-                    <th>Serial Number</th>
-                    <th>Buyer</th>
-                    <th>Price</th>
-                </tr>
-                </thead>
+        <tbody>
+          {invoiceList.map((invoice) => (
+            <tr key={invoice.id}>
+              <td>{invoice.invoiceDate}</td>
+              {/*TODO: Change invoice.expirationDate to invoice.expiryDate when axios is implemented*/}
+              <td>{invoice.expirationDate}</td>
+              {/*TODO: Add status when backend has it*/}
+              <td>PENDING</td>
+              <td>{invoice.id}</td>
+              <td>{invoice.customerName}</td>
+              <td>{sumOfProducts(invoice.items)}</td>
+              <td>
+                <button
+                  className="btn btn-info"
+                  onClick={() => loadPreview(invoice)}
+                >
+                  Preview
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
 
-                <tbody>
-                {
-                    invoiceList.map(
-                        (invoice) => (
-                            <tr key={invoice.id}>
-                                <td>{invoice.invoiceDate}</td>
-                                {/*TODO: Change invoice.expirationDate to invoice.expiryDate when axios is implemented*/}
-                                <td>{invoice.expirationDate}</td>
-                                {/*TODO: Add status when backend has it*/}
-                                <td>PENDING</td>
-                                <td>{invoice.id}</td>
-                                <td>{invoice.customerName}</td>
-                                <td>{sumOfProducts(invoice.items)}</td>
-                                <td>
-                                    <button className="btn btn-info" onClick={() => loadPreview(invoice)}>
-                                        Preview
-                                    </button>
-                                </td>
-                            </tr>
-                        )
-                    )
-                }
-                </tbody>
-            </Table>
+      <button onClick={goBackToHomePage} className="btn btn-info">
+        Return
+      </button>
+    </div>
+  );
+};
 
-            <button onClick={goBackToHomePage} className="btn btn-info">Return</button>
-        </div>
-    )
-}
-
-export default InvoiceList
+export default InvoiceList;
--- src/components/seeInvoicesComponents/PreviewInvoice.js
+++ src/components/seeInvoicesComponents/PreviewInvoice.js
@@ -1,104 +1,124 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import logo from '../../logo.png';
+import React from "react";
+import ReactDOM from "react-dom";
+import logo from "../../logo.png";
 
-import {Page, Document, Text, StyleSheet, PDFViewer, Font, Image, View} from "@react-pdf/renderer"
+import {
+  Page,
+  Document,
+  Text,
+  StyleSheet,
+  PDFViewer,
+  Font,
+  Image,
+  View,
+} from "@react-pdf/renderer";
 import InvoiceTitle from "./pdfComponents/InvoiceTitle";
 import InvoiceNo from "./pdfComponents/InvoiceNo";
 import BillTo from "./pdfComponents/BillTo";
 import InvoiceItemsTable from "./pdfComponents/InvoiceItemsTable2";
 
 const invoice = {
-    id: "5df3180a09ea16dc4b95f910",
-    invoice_no: "201906-28",
-    balance: "$2,283.74",
-    company: "MANTRIX",
-    email: "susanafuentes@mantrix.com",
-    phone: "+1 (872) 588-3809",
-    address: "922 Campus Road, Drytown, Wisconsin, 1986",
-    trans_date: "2019-09-12",
-    due_date: "2019-10-12",
-    items: [
-        {
-            sno: 1,
-            desc: "ad sunt culpa occaecat qui",
-            qty: 5,
-            rate: 405.89,
-        },
-        {
-            sno: 2,
-            desc: "cillum quis sunt qui aute",
-            qty: 5,
-            rate: 373.11,
-        },
-        {
-            sno: 3,
-            desc: "ea commodo labore culpa irure",
-            qty: 5,
-            rate: 458.61,
-        },
-        {
-            sno: 4,
-            desc: "nisi consequat et adipisicing dolor",
-            qty: 10,
-            rate: 725.24,
-        },
-        {
-            sno: 5,
-            desc: "proident cillum anim elit esse",
-            qty: 4,
-            rate: 141.02,
-        },
-    ],
+  id: "5df3180a09ea16dc4b95f910",
+  invoice_no: "201906-28",
+  balance: "$2,283.74",
+  company: "MANTRIX",
+  email: "susanafuentes@mantrix.com",
+  phone: "+1 (872) 588-3809",
+  address: "922 Campus Road, Drytown, Wisconsin, 1986",
+  trans_date: "2019-09-12",
+  due_date: "2019-10-12",
+  items: [
+    {
+      sno: 1,
+      desc: "ad sunt culpa occaecat qui",
+      qty: 5,
+      rate: 405.89,
+    },
+    {
+      sno: 2,
+      desc: "cillum quis sunt qui aute",
+      qty: 5,
+      rate: 373.11,
+    },
+    {
+      sno: 3,
+      desc: "ea commodo labore culpa irure",
+      qty: 5,
+      rate: 458.61,
+    },
+    {
+      sno: 4,
+      desc: "nisi consequat et adipisicing dolor",
+      qty: 10,
+      rate: 725.24,
+    },
+    {
+      sno: 5,
+      desc: "proident cillum anim elit esse",
+      qty: 4,
+      rate: 141.02,
+    },
+  ],
 };
 
-const PreviewInvoice = (data) => (
-    <div>
-        <PDFViewer style={{
-            minHeight: "800px",
-            minWidth: "800px",
-            height: "99%",
-            width: "100%",
-            border: "0px",
-            display: "flex",
-            overflow: "hidden"
-        }}>
-            <Document>
-                <Page size="A4" style={styles.page}>
-                    <Image style={styles.logo} src={logo}/>
-                    <InvoiceTitle title='Invoice'/>
-                    <InvoiceNo invoice={invoice}/>
-                    <BillTo invoice={invoice}/>
-                    <InvoiceItemsTable invoice={invoice}/>
-                </Page>
-            </Document>
-        </PDFViewer>
-    </div>
-);
-
 Font.register({
-    family: 'Oswald',
-    src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf'
+  family: "Oswald",
+  src: "https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf",
 });
 
 const styles = StyleSheet.create({
-    page: {
-        fontFamily: 'Helvetica',
-        fontSize: 11,
-        paddingTop: 30,
-        paddingLeft:60,
-        paddingRight:60,
-        lineHeight: 1.5,
-        flexDirection: 'column',
-    },
-    logo: {
-        width: 74,
-        height: 66,
-        marginLeft: 'auto',
-        marginRight: 'auto'
-    }
+  page: {
+    fontFamily: "Helvetica",
+    fontSize: 11,
+    paddingTop: 30,
+    paddingLeft: 60,
+    paddingRight: 60,
+    lineHeight: 1.5,
+    flexDirection: "column",
+  },
+  logo: {
+    width: 74,
+    height: 66,
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
 });
 
-ReactDOM.render(<PreviewInvoice/>, document.getElementById('root'));
+const PreviewInvoice = (props) => {
+  {
+    /* TODO: You can now access the the the invoice details through props.
+        i.e <div>CUSTOMER NAME: {props.customerName}</div>
+            <div>CUSTOMER EMAIL: {props.customerEmail}</div>
+    */
+  }
 
-export default PreviewInvoice
+  return (
+    <div>
+      <PDFViewer
+        style={{
+          minHeight: "800px",
+          minWidth: "800px",
+          height: "99%",
+          width: "100%",
+          border: "0px",
+          display: "flex",
+          overflow: "hidden",
+        }}
+      >
+        <Document>
+          <Page size="A4" style={styles.page}>
+            <Image style={styles.logo} src={logo} />
+            <InvoiceTitle title="Invoice" />
+            <InvoiceNo invoice={invoice} />
+            <BillTo invoice={invoice} />
+            <InvoiceItemsTable invoice={invoice} />
+          </Page>
+        </Document>
+      </PDFViewer>
+    </div>
+  );
+};
+
+ReactDOM.render(<PreviewInvoice />, document.getElementById("root"));
+
+export default PreviewInvoice;
