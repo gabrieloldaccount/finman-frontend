8eaf0f1feee4f17595961342af5ba6e5d08e23a9
Gabriel <gabriel.brattgard@gmail.com>
2021-05-06 11:39:26 +0200

    Added ValidateZipcode for AddInvoice.js. Checks for exactly 5 numbers and trims away all spaces and dashes.
    Co-authored-by: Puh00 <alex.phu@live.com>

--- src/components/AddInvoice.js
+++ src/components/AddInvoice.js
@@ -1,55 +1,78 @@
-import { useState } from "react";
+import {useState} from "react";
 import AddItem from "./AddItem";
 import Items from "./Items";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { Col, Container, Row } from "react-bootstrap";
-import { pdf } from "@react-pdf/renderer";
-import PdfDocument from "./seeInvoicesComponents/pdfComponents/PdfDocument";
-import InvoiceService from "../api-services/InvoiceService";
+import {Col, Container, Row} from "react-bootstrap";
 
 const blobToPdf = (blob, fileName) => {
-  blob.lastModifiedDate = new Date();
-  blob.name = fileName;
-  console.log("SIZE: " + blob.size);
-  console.log("TYPE: " + blob.type);
-  return blob;
+    blob.lastModifiedDate = new Date();
+    blob.name = fileName;
+    console.log("SIZE: " + blob.size);
+    console.log("TYPE: " + blob.type);
+    return blob;
 };
 
-const AddInvoice = ({ owner, productList }) => {
-  const [name, setName] = useState("TestPerson Persson");
-  const [address, setAddress] = useState("Hellroad");
-  const [zipcode, setZipCode] = useState("54512");
-  const [email, setEmail] = useState("hellmail@devil.com");
-  const [invoiceDate, setInvoiceDate] = useState("2021-04-30");
-  const [expirationDate, setExpirationDate] = useState("2021-05-02");
-  const [items, setItems] = useState([]);
-  const [city, setCity] = useState("Gothenburg");
-  const [country, setCountry] = useState("Norway");
-  const [telephone, setTelephone] = useState("06535342");
+const validateEmail = (email) => {
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) {
+        return true
+    }
+    alert("You have entered an invalid email address!")
+    return false
+}
 
-  const invoiceData = {
-    source: owner,
-    invoiceDate: invoiceDate,
-    expiryDate: expirationDate,
-    seller: "finman@block.chain",
-    customer: {
-      name: name,
-      address: address,
-      zipCode: zipcode,
-      city: city,
-      country: country,
-      telephone: telephone,
-      email: email,
-    },
-    items: items,
-  };
+const validateZipcode = (zipcode) => {
+    const formatted = zipcode.trim().replace(/[-]/g, "").replaceAll(" ", "");
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
+    if (!hasLetters(formatted) && formatted.length === 5) {
+        return true
+    }
+    alert("You have entered an invalid zipcode!")
+    return false
+}
+
+const hasNumber = (text) => {
+    return /\d/.test(text);
+}
+
+const hasLetters = (text) => {
+    return /[a-zA-Z]/.test(text);
+}
+
+const AddInvoice = ({owner, productList}) => {
+    const [name, setName] = useState("TestPerson Persson");
+    const [address, setAddress] = useState("Hellroad");
+    const [zipcode, setZipCode] = useState("54512");
+    const [email, setEmail] = useState("hellmail@devil.com");
+    const [invoiceDate, setInvoiceDate] = useState("2021-04-30");
+    const [expirationDate, setExpirationDate] = useState("2021-05-02");
+    const [items, setItems] = useState([]);
+    const [city, setCity] = useState("Gothenburg");
+    const [country, setCountry] = useState("Norway");
+    const [telephone, setTelephone] = useState("06535342");
+
+    const invoiceData = {
+        source: owner,
+        invoiceDate: invoiceDate,
+        expiryDate: expirationDate,
+        seller: "finman@block.chain",
+        customer: {
+            name: name,
+            address: address,
+            zipCode: zipcode,
+            city: city,
+            country: country,
+            telephone: telephone,
+            email: email,
+        },
+        items: items,
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
 
         //TODO: Add more error-checking here.
-        if (ValidateEmail(email)) {
+        if (validateZipcode(zipcode)) {
             // Extract pdf
             /*let pdfBlob = await pdf(<PdfDocument invoice={invoiceData}/>).toBlob();
             pdfBlob = blobToPdf(pdfBlob, "invoice.pdf");
@@ -73,165 +96,144 @@ const AddInvoice = ({ owner, productList }) => {
             setItems([]);
         }
 
-    // Extract pdf
-    let pdfBlob = await pdf(<PdfDocument invoice={invoiceData} />).toBlob();
-    pdfBlob = blobToPdf(pdfBlob, "invoice.pdf");
+    };
 
-    // Create and send Invoice
-    InvoiceService.createInvoice(
-      invoiceData,
-      pdfBlob,
-      invoiceData.customer.email
+    //Adds an item to this invoice's items list
+    const addItem = (item) => {
+        setItems([...items, item]);
+    };
+
+    //Deletes an item from the invoice's items list
+    const deleteItem = (id) => {
+        setItems(items.filter((item) => item.id !== id));
+    };
+
+    const sumOfProducts = (items) => {
+        let sum = 0;
+        items.forEach((product) => (sum += product.price * product.amount));
+        return sum;
+    };
+
+    return (
+        <Container>
+            {/*TODO: Format this whole Form-thing into just plain stuff instead. Don't think we gain anything from the form format.*/}
+            <Form>
+                <Row>
+                    <Col>
+                        <Form.Group controlId="formCustomerName">
+                            <Form.Label>Customer Name</Form.Label>
+                            <Form.Control
+                                type="string"
+                                value={name}
+                                placeholder="Enter customer name"
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="formCustomerEmail">
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control
+                                type="string"
+                                value={address}
+                                placeholder="Enter address"
+                                onChange={(e) => setAddress(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="formCustomerEmail">
+                            <Form.Label>Zipcode</Form.Label>
+                            <Form.Control
+                                type="string"
+                                value={zipcode}
+                                placeholder="Enter zip code"
+                                onChange={(e) => setZipCode(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="formCustomerEmail">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control
+                                type="string"
+                                value={city}
+                                placeholder="Enter city"
+                                onChange={(e) => setCity(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="formCustomerEmail">
+                            <Form.Label>Telephone</Form.Label>
+                            <Form.Control
+                                type="number"
+                                value={telephone}
+                                placeholder="Enter telephone number"
+                                onChange={(e) => setTelephone(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group controlId="formInvoiceDate">
+                            <Form.Label>Invoice Date</Form.Label>
+                            <Form.Control
+                                type="date"
+                                value={invoiceDate}
+                                onChange={(e) => setInvoiceDate(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formExpirationDate">
+                            <Form.Label>Expiration Date</Form.Label>
+                            <Form.Control
+                                type="date"
+                                value={expirationDate}
+                                onChange={(e) => setExpirationDate(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="formCustomerEmail">
+                            <Form.Label>Country</Form.Label>
+                            <Form.Control
+                                type="string"
+                                value={country}
+                                placeholder="Enter country"
+                                onChange={(e) => setCountry(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="formCustomerEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type="email"
+                                value={email}
+                                placeholder="Enter Customer Email"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row className="row-create-item">
+                    <AddItem
+                        owner={owner}
+                        productList={productList}
+                        onAddItem={addItem}
+                    />
+                </Row>
+                <Row>
+                    {items.length > 0 ? (
+                        <Items items={items} onDelete={deleteItem}/>
+                    ) : (
+                        "No Items To Show "
+                    )}
+                </Row>
+
+                <Row>{"Total: " + sumOfProducts(items)}</Row>
+
+                <Row>
+                    <Button variant="primary" type="submit" onClick={onSubmit}>
+                        Send invoice
+                    </Button>
+                </Row>
+            </Form>
+        </Container>
     );
-
-    setName("");
-    setAddress("");
-    setZipCode("");
-    setEmail("");
-    setInvoiceDate("");
-    setExpirationDate("");
-    setTelephone("");
-    setCountry("");
-    setCity("");
-    setItems([]);
-  };
-
-  //Adds an item to this invoice's items list
-  const addItem = (item) => {
-    setItems([...items, item]);
-  };
-
-  //Deletes an item from the invoice's items list
-  const deleteItem = (id) => {
-    setItems(items.filter((item) => item.id !== id));
-  };
-
-  const sumOfProducts = (items) => {
-    let sum = 0;
-    items.forEach((product) => (sum += product.price * product.amount));
-    return sum;
-  };
-
-  return (
-    <Container>
-      {/*TODO: Format this whole Form-thing into just plain stuff instead. Don't think we gain anything from the form format.*/}
-      <Form>
-        <Row>
-          <Col>
-            <Form.Group controlId="formCustomerName">
-              <Form.Label>Customer Name</Form.Label>
-              <Form.Control
-                type="string"
-                value={name}
-                placeholder="Enter customer name"
-                onChange={(e) => setName(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group controlId="formCustomerEmail">
-              <Form.Label>Address</Form.Label>
-              <Form.Control
-                type="string"
-                value={address}
-                placeholder="Enter address"
-                onChange={(e) => setAddress(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group controlId="formCustomerEmail">
-              <Form.Label>Zipcode</Form.Label>
-              <Form.Control
-                type="number"
-                value={zipcode}
-                placeholder="Enter zip code"
-                onChange={(e) => setZipCode(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group controlId="formCustomerEmail">
-              <Form.Label>City</Form.Label>
-              <Form.Control
-                type="string"
-                value={city}
-                placeholder="Enter city"
-                onChange={(e) => setCity(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group controlId="formCustomerEmail">
-              <Form.Label>Telephone</Form.Label>
-              <Form.Control
-                type="number"
-                value={telephone}
-                placeholder="Enter telephone number"
-                onChange={(e) => setTelephone(e.target.value)}
-              />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId="formInvoiceDate">
-              <Form.Label>Invoice Date</Form.Label>
-              <Form.Control
-                type="date"
-                value={invoiceDate}
-                onChange={(e) => setInvoiceDate(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="formExpirationDate">
-              <Form.Label>Expiration Date</Form.Label>
-              <Form.Control
-                type="date"
-                value={expirationDate}
-                onChange={(e) => setExpirationDate(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group controlId="formCustomerEmail">
-              <Form.Label>Country</Form.Label>
-              <Form.Control
-                type="string"
-                value={country}
-                placeholder="Enter country"
-                onChange={(e) => setCountry(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group controlId="formCustomerEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                value={email}
-                placeholder="Enter Customer Email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row className="row-create-item">
-          <AddItem
-            owner={owner}
-            productList={productList}
-            onAddItem={addItem}
-          />
-        </Row>
-        <Row>
-          {items.length > 0 ? (
-            <Items items={items} onDelete={deleteItem} />
-          ) : (
-            "No Items To Show "
-          )}
-        </Row>
-
-        <Row>{"Total: " + sumOfProducts(items)}</Row>
-
-        <Row>
-          <Button variant="primary" type="submit" onClick={onSubmit}>
-            Send invoice
-          </Button>
-        </Row>
-      </Form>
-    </Container>
-  );
 };
 
 export default AddInvoice;
