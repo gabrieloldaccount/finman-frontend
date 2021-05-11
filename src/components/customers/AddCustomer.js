f4874b6829ae58808929e063754a7f2a4f07b336
Gabriel <gabriel.brattgard@gmail.com>
2021-05-11 18:53:07 +0200

    Restructured a bit in AddCustomer.js

--- src/components/customers/AddCustomer.js
+++ src/components/customers/AddCustomer.js
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 import Button from "react-bootstrap/Button";
 import {Col, Container, Row} from "react-bootstrap";
 
-const AddCustomer = ({ onAdd }) => {
+const AddCustomer = ({onAdd}) => {
     const [name, setName] = useState("TestPerson Persson");
     const [address, setAddress] = useState("Hellroad");
     const [zipcode, setZipCode] = useState("54512");
@@ -12,120 +12,73 @@ const AddCustomer = ({ onAdd }) => {
     const [country, setCountry] = useState("Norway");
     const [telephone, setTelephone] = useState("06535342");
 
+    //TODO: Implement onAdd properly with backend.
     const onSubmit = async (e) => {
         e.preventDefault();
 
-            onAdd({
-                customer: {
-                    name: name,
-                    address: address,
-                    zipCode: zipcode,
-                    city: city,
-                    country: country,
-                    telephone: telephone,
-                    email: email,
-                },
-            })
+        onAdd({
+            customer: {
+                name: name,
+                address: address,
+                zipCode: zipcode,
+                city: city,
+                country: country,
+                telephone: telephone,
+                email: email,
+            },
+        })
 
-            setName("");
-            setAddress("");
-            setZipCode("");
-            setEmail("");
-            setTelephone("");
-            setCountry("");
-            setCity("");
+        setName("");
+        setAddress("");
+        setZipCode("");
+        setEmail("");
+        setTelephone("");
+        setCountry("");
+        setCity("");
     }
 
-    return(
+    return (
         <Container>
-            <Form>
-                <Row>
-                    <Col>
-                        <Form.Group controlId="formCustomerName">
-                            <Form.Label className="invoice-label">Customer Name</Form.Label>
-                            <Form.Control
-                                type="string"
-                                value={name}
-                                placeholder="Enter customer name"
-                                onChange={(e) => setName(e.target.value)}
-                            />
-                        </Form.Group>
+            <Row>
+                <Col>
+                    <Form.Label className="invoice-label">Customer Name</Form.Label>
+                    <Form.Control type="string" value={name} placeholder="Enter customer name"
+                                  onChange={(e) => setName(e.target.value)}/>
 
-                        <Form.Group controlId="formCustomerEmail">
-                            <Form.Label className="invoice-label">Telephone</Form.Label>
-                            <Form.Control
-                                type="number"
-                                value={telephone}
-                                placeholder="Enter telephone number"
-                                onChange={(e) => setTelephone(e.target.value)}
-                            />
-                        </Form.Group>
+                    <Form.Label className="invoice-label">Telephone</Form.Label>
+                    <Form.Control type="number" value={telephone} placeholder="Enter telephone number"
+                                  onChange={(e) => setTelephone(e.target.value)}/>
 
-                        <Form.Group controlId="formCustomerEmail">
-                            <Form.Label className="invoice-label">Email</Form.Label>
-                            <Form.Control
-                                type="email"
-                                value={email}
-                                placeholder="Enter Customer Email"
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </Form.Group>
+                    <Form.Label className="invoice-label">Email</Form.Label>
+                    <Form.Control type="email" value={email} placeholder="Enter Customer Email"
+                                  onChange={(e) => setEmail(e.target.value)}/>
+                </Col>
 
-                    </Col>
-                    <Col>
+                <Col>
+                    <Form.Label className="invoice-label">Address</Form.Label>
+                    <Form.Control type="string" value={address} placeholder="Enter address"
+                                  onChange={(e) => setAddress(e.target.value)}/>
 
-                        <Form.Group controlId="formCustomerEmail">
-                            <Form.Label className="invoice-label">Address</Form.Label>
-                            <Form.Control
-                                type="string"
-                                value={address}
-                                placeholder="Enter address"
-                                onChange={(e) => setAddress(e.target.value)}
-                            />
-                        </Form.Group>
+                    <Form.Label className="invoice-label">Zipcode</Form.Label>
+                    <Form.Control type="string" value={zipcode} placeholder="Enter zip code"
+                                  onChange={(e) => setZipCode(e.target.value)}/>
 
-                        <Form.Group controlId="formCustomerEmail">
-                            <Form.Label className="invoice-label">Zipcode</Form.Label>
-                            <Form.Control
-                                type="string"
-                                value={zipcode}
-                                placeholder="Enter zip code"
-                                onChange={(e) => setZipCode(e.target.value)}
-                            />
-                        </Form.Group>
+                    <Form.Label className="invoice-label">City</Form.Label>
+                    <Form.Control type="string" value={city} placeholder="Enter city"
+                                  onChange={(e) => setCity(e.target.value)}/>
 
-                        <Form.Group controlId="formCustomerEmail">
-                            <Form.Label className="invoice-label">City</Form.Label>
-                            <Form.Control
-                                type="string"
-                                value={city}
-                                placeholder="Enter city"
-                                onChange={(e) => setCity(e.target.value)}
-                            />
-                        </Form.Group>
-
-                        <Form.Group controlId="formCustomerEmail">
-                            <Form.Label className="invoice-label">Country</Form.Label>
-                            <Form.Control
-                                type="string"
-                                value={country}
-                                placeholder="Enter country"
-                                onChange={(e) => setCountry(e.target.value)}
-                            />
-                        </Form.Group>
-
-                    </Col>
-                </Row>
-                <Row>
-                    <Button
-                    variant="primary"
-                    type="submit"
-                    onClick={onSubmit}
-                    className={"marginBottom"}>
+                    <Form.Label className="invoice-label">Country</Form.Label>
+                    <Form.Control type="string" value={country} placeholder="Enter country"
+                                  onChange={(e) => setCountry(e.target.value)}/>
+                </Col>
+            </Row>
+            <Row>
+                <Col md="10">
+                    <Button variant="primary" type="submit" onClick={onSubmit} className={"marginBottom"}>
                         Add Customer
                     </Button>
-                </Row>
-            </Form>
+                </Col>
+            </Row>
         </Container>
     )
 }
