da50910db0cdc1e20366079a0774ad3f0d69e2c8
stinahagl <81407388+stinahagl@users.noreply.github.com>
2021-05-18 13:51:20 +0200

    Changes by elinfredriksson

--- src/components/customers/AddCustomer.js
+++ src/components/customers/AddCustomer.js
@@ -2,6 +2,50 @@ import Form from "react-bootstrap/Form";
 import React, {useState} from "react";
 import Button from "react-bootstrap/Button";
 import {Col, Container, Row} from "react-bootstrap";
+import {number} from "prop-types";
+
+const hasNumber = (text) => {
+    return /\d/.test(text);
+};
+
+const hasLetters = (text) => {
+    return /[a-zA-Z]/.test(text);
+};
+
+const validateEmail = (email) => {
+    if (
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+            email
+        )
+    ) {
+        return true;
+    }
+    alert("You have entered an invalid email address!");
+    return false;
+};
+
+const validateZipcode = (zipcode) => {
+    const formatted = zipcode.trim().replace(/[-]/g, "").replaceAll(" ", "");
+
+    if (!hasLetters(formatted) && formatted.length === 5) {
+        return true;
+    }
+    alert("You have entered an invalid zipcode!");
+    return false;
+};
+
+const validateTelephone = (number) => {
+    const formatted = number
+        .toString()
+        .trim()
+        .replace(/[-]/g, "")
+        .replaceAll(" ", "");
+
+    if (hasLetters(formatted)) return true;
+
+    alert("You have entered an invalid email address!");
+    return false;
+};
 
 const AddCustomer = ({onAdd}) => {
     const [name, setName] = useState("TestPerson Persson");
@@ -16,25 +60,37 @@ const AddCustomer = ({onAdd}) => {
     const onSubmit = async (e) => {
         e.preventDefault();
 
-        onAdd({
-            customer: {
-                name: name,
-                address: address,
-                zipCode: zipcode,
-                city: city,
-                country: country,
-                telephone: telephone,
-                email: email,
-            },
-        })
+        if (
+            validateEmail(email) &&
+            validateZipcode(zipcode) &&
+            validateTelephone(number) &&
+            !hasNumber(name) &&
+            !hasNumber(country) &&
+            !hasNumber(city)
+        ) {
+            onAdd({
+                customer: {
+                    name: name,
+                    address: address,
+                    zipCode: zipcode,
+                    city: city,
+                    country: country,
+                    telephone: telephone,
+                    email: email,
+                },
+            })
 
-        setName("");
-        setAddress("");
-        setZipCode("");
-        setEmail("");
-        setTelephone("");
-        setCountry("");
-        setCity("");
+            setName("");
+            setAddress("");
+            setZipCode("");
+            setEmail("");
+            setTelephone("");
+            setCountry("");
+            setCity("");
+        }else {
+            alert("Please fill in the correct input");
+        }
+
     }
 
     return (
@@ -74,7 +130,22 @@ const AddCustomer = ({onAdd}) => {
             </Row>
             <Row>
                 <Col md="10">
-                    <Button variant="primary" type="submit" onClick={onSubmit} className={"marginBottom"}>
+                    <Button
+                        disabled={
+                            !(
+                                name &&
+                                city &&
+                                address &&
+                                zipcode &&
+                                email &&
+                                country &&
+                                telephone
+                            )
+                        }
+                        variant="primary"
+                        type="submit"
+                        onClick={onSubmit}
+                        className={"marginBottom"}>
                         Add Customer
                     </Button>
                 </Col>
